0d3c4b599d6c98a8515071064fffcc499c9349af
Joel MacFadyen <jamage.art@gmail.com>
2016-12-19 21:40:33 -0500

    First commit!
    Please work

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+
+    [Header("Components")]
+    public Rigidbody charRigidbody;
+
+    [Header("Char Stats")]
+    public float charSpeed;
+    public float charJump;
+
+    public enum charState
+    {
+        Normal,
+        Mirror,
+        AntiGrav,
+        Async,
+        Single
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if(Input.GetAxis("Horizontal") > 0)
+        {
+            charRigidbody.AddForce(charSpeed, 0, 0);
+        }
+        if(Input.GetAxis("Horizontal") < 0)
+        {
+            charRigidbody.AddForce(-charSpeed, 0, 0);
+        }
+
+	}
+}
